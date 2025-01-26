1bef84cce6aa8e90852f1c5633bdda6bc1ce8ae4
Joao Paulo <joaopaulloss477@gmail.com>
2025-01-26 16:25:13 -0300

    Adding - Models

--- task_manager/tasks/models.py
+++ task_manager/tasks/models.py
@@ -1,3 +1,13 @@
-from django.db import models
+from django.db import models # type: ignore
+from django.contrib.auth.models import User # type: ignore
 
-# Create your models here.
+class Task(models.Model):
+  title = models.CharField(max_length=200)
+  description = models.TextField()
+  completed = models.BooleanField(default=False)
+  created_at = models.DateTimeField(auto_now_add=True)
+  due_date = models.DateTimeField()
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
+  
+  def __str__(self):
+    return self.title
